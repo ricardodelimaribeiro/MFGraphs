055d053dbfb8cf3c677b0182d165d69a3f7ee710
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2020-05-11 11:59:11 +0300

    Fixing the merge and including some usage messages for functions.

--- MFGraphs/NetworkBuild.m
+++ MFGraphs/NetworkBuild.m
@@ -1,9 +1,7 @@
 (* Wolfram Language package *)
 
 DataToGraph[Data_?AssociationQ] :=
-<<<<<<< HEAD
-
-    
+   
     Module[{BG, EntranceVertices, InwardVertices, InEdges, ExitVertices, OutwardVertices, OutEdges, AuxiliaryGraph, FG},
     BG = AdjacencyGraph[Data["Vertices List"], Data["Adjacency Matrix"], VertexLabels -> "Name"];
     EntranceVertices = First /@ Data["Entrance Vertices and Currents"];
@@ -27,31 +25,3 @@ DataToGraph[Data_?AssociationQ] :=
     	"FG" -> FG
     	 }]
     ]
-(*TODO finish the association thing. DONE*)
-=======
-    
-    Module[{BG, EntranceVertices, InwardVertices, InEdges, ExitVertices, OutwardVertices, OutEdges, AuxiliaryGraph, FG},
-    BG = AdjacencyGraph[Data["Vertices List"], Data["Adjacency Matrix"], VertexLabels -> "Name"];
-    EntranceVertices = First /@ Data["Entrance Vertices and Currents"];
-    InwardVertices = AssociationThread[EntranceVertices, Unique["en"] & /@ EntranceVertices];
-    (*Defines auxiliary vertices for the entrance vertices*)
-    InEdges = MapThread[DirectedEdge, {InwardVertices /@ EntranceVertices, EntranceVertices}];
-    ExitVertices = First /@ Data["Exit Vertices and Terminal Costs"];
-    OutwardVertices = AssociationThread[ExitVertices, Unique["ex"] & /@ ExitVertices];
-    OutEdges = MapThread[DirectedEdge, {ExitVertices, OutwardVertices /@ ExitVertices}];
-    AuxiliaryGraph = Graph[Join[InEdges, OutEdges], VertexLabels -> "Name"];
-    FG = EdgeAdd[BG, Join[InEdges, OutEdges]];
-    Association[{
-    	"BG" -> BG, 
-    	"EntranceVertices" -> EntranceVertices, 
-    	"InwardVertices" -> InwardVertices,
-    	"InEdges" -> InEdges, 
-    	"ExitVertices" -> ExitVertices, 
-    	"OutwardVertices" -> OutwardVertices, 
-    	"OutEdges" -> OutEdges, 
-    	"AuxiliaryGraph" -> AuxiliaryGraph, 
-    	"FG" -> FG
-    	 }]
-    ]
-(*TODO finish the association thing.*)
->>>>>>> branch 'master' of https://github.com/ricardodelimaribeiro/MFGraphs
