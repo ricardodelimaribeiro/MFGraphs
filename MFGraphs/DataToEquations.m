7344bb71a97579c86e1f9ba0f74e93ac453edfc9
fatimah al saleh <fatimah.saleh@kaust.edu.sa>
2020-05-12 00:28:42 +0300

    Removing the functions from association.

--- MFGraphs/DataToEquations.m
+++ MFGraphs/DataToEquations.m
@@ -3,8 +3,9 @@
 DataToEquations::usage = "takes data assotiation to the equations";
 
 Begin["`Private`"]
-(*TODO remove (most) functions from the association*)
+(*TODO remove (most) functions from the association DONE*)
 (*TODO how do we use usage message to make template on the notebook*)
+
 DataToEquations::usage = 
 "DataToEquations[<|\"Vertices List\"->{x}|>] Inputs the data as an association and returns the equations for the stationary mean-field game on the network, except for the coupling equations.";
 
@@ -18,9 +19,6 @@ DataToEquations[Data_?AssociationQ] :=
     OutEdges, 
     AuxiliaryGraph, 
     FG,
-    AtHead, 
-    AtTail , 
-    TransitionsAt, OtherWay, triple2path,
     VL, EL, BEL, jargs, 
     js, jvars, FVL, 
     AllTransitions, jts, jtvars, 
@@ -71,61 +69,6 @@ DataToEquations[Data_?AssociationQ] :=
         AuxiliaryGraph = 
          Graph[Join[InEdges, OutEdges], VertexLabels -> "Name"];
         FG = EdgeAdd[BG, Join[InEdges, OutEdges]];
-        AtHead[a_ \[DirectedEdge] b_] :=
-            {b, a \[DirectedEdge] b};
-        AtTail[a_ \[DirectedEdge] b_] :=
-            {a, a \[DirectedEdge] b};
-        TransitionsAt[G_, k_] :=
-            Prepend[#, k] & /@ Permutations[IncidenceList[G, k], {2}];
-        OtherWay[{c_, a_ \[DirectedEdge] b_}] :=
-            {If[ c === a,
-                 b,
-                 a
-             ],  a \[DirectedEdge] b}(*example:OtherWay[{a,a\[DirectedEdge]b}]={b,a\[DirectedEdge]b}\
-       *);
-        triple2path[{a_, b_, c_}, 
-          G_] :=(*Takes 3 ordered vertices and gives the pair of edges \
-       conecting the first two and the last two.If this is not feasible,
-         error message with {a,b,c}!*)
-            (EL = EdgeList[FG];
-             If[ SubsetQ[AdjacencyList[G, b], {a, c}],
-                 If[ MemberQ[EL, a \[DirectedEdge] b],
-                     If[ MemberQ[EL, b \[DirectedEdge] c],
-                         {b, a \[DirectedEdge] b, 
-                         b \[DirectedEdge] c},
-                         {b, a \[DirectedEdge] b, 
-                         c \[DirectedEdge] b}
-                     ],
-                     If[ MemberQ[EL, b \[DirectedEdge] a],
-                         If[ MemberQ[EL, b \[DirectedEdge] c],
-                             {b, b \[DirectedEdge] a, 
-                             b \[DirectedEdge] c},
-                             {b, b \[DirectedEdge] a, 
-                             c \[DirectedEdge] b}
-                         ]
-                     ]
-                 ],
-                 StringJoin["\n There is no path from ", ToString[a], " to ", 
-                  ToString[b], " to ", ToString[c]]
-             ]);
-        (*DifferenceU[edge_]:=(H[x_,p_,m_,alpha_]:=p^2/(2 \
-       m^alpha)+V[x]-g[m];
-        HJ=(H[x,D[u[x],x],m[x],alpha]\[Equal]0);
-        curr=((j\[Equal]-m[x]*D[H[x,p,m[x],alpha],p])/.p\[Rule]D[u[x],x]);
-        ux=(Solve[curr,u'[x]]//Flatten);
-        (*R=(HJ/.(curr/.Equal\[Rule]Rule)/.ux);*)R=(HJ/.ux);
-        sol=RSolve[R,m[x],x]//Flatten;
-        M=Select[Select[sol,Im[Values[#/.{x\[Rule].4,j\[Rule]1}]]\[Equal]0&],\
-       Re[Values[#/.{x\[Rule].4,j\[Rule]1}]]\[GreaterEqual]0&];(*Looks \
-       good!*)(*One way of selecting,is there a better \
-       one?*)mfunc[edge]=M/.{j\[Rule]jvars[AtHead[edge]]-jvars[AtTail[edge]]};
-        fux[edge]=ux/.M/.{j\[Rule]jvars[AtHead[edge]]-jvars[AtTail[edge]]};
-        (*Integrate[(u'[x]/.fux),{x,0,1}]*)h[y_]:=Values[fux[edge]]/.x\[Rule]\
-       y;
-        ufunc[edge]={u[y]\[Rule]uvars[AtTail[edge]]-(jvars[AtHead[edge]]-\
-       jvars[AtTail[edge]]) Integrate[m[x]^(alpha-1)/.mfunc[edge],{x,0,y}]};
-        -(jvars[AtHead[edge]]-jvars[AtTail[edge]]) \
-       Integrate[m[x]^(alpha-1)/.mfunc[edge],{x,0,1}]);*)
         VL = Data["Vertices List"];
         EL = EdgeList[FG];
         BEL = EdgeList[BG];
@@ -240,7 +183,7 @@ DataToEquations[Data_?AssociationQ] :=
           And @@ ((uvars[AtHead[#]] - uvars[AtTail[#]] == 0) & /@ 
              EdgeList[AuxiliaryGraph]);
         EqNoInt = EqAll && EqValueAuxiliaryEdges;
-        EqAll = EqAll && EqValue && EqValueAuxiliaryEdges;
+      (*  EqAll = EqAll && EqValue && EqValueAuxiliaryEdges;*)
         Association[{
             "BG" -> BG, 
             "EntranceVertices" -> EntranceVertices, 
@@ -251,11 +194,6 @@ DataToEquations[Data_?AssociationQ] :=
           "OutEdges" -> OutEdges, 
           "AuxiliaryGraph" -> AuxiliaryGraph, 
           "FG" -> FG, 
-          "AtHead[a\[DirectedEdge]b]" -> AtHead, 
-          "AtTail[a\[DirectedEdge]b]" -> AtTail, 
-          "TransitionsAt[G,k]" -> TransitionsAt, 
-          "OtherWay[{c,a\[DirectedEdge]b}]" -> OtherWay, 
-          "triple2path[{a, b, c}, G]" -> triple2path,
           "VL" -> VL, 
           "EL" -> EL, 
           "BEL" -> BEL, 
