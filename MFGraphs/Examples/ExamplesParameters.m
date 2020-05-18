1a88f16b4ef0cb5988aea888a3e92337666e1c54
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2020-05-18 13:37:51 +0300

    Turning the Parameters Association into a Symbol and making it give pure functions.

--- MFGraphs/Examples/ExamplesParameters.m
+++ MFGraphs/Examples/ExamplesParameters.m
@@ -1,20 +1,19 @@
 (* Wolfram Language package *)
 (* this file looks like what we want.*)
-Parameters :=
+Parameters =
     Module[ {alpha, beta, g, W, V, H},
         alpha = 1;
         beta = 2;
-        g[m_] :=
-            If[ beta == 0,
-                Log[m],
-                m^beta
+        g = If[ beta == 0, 
+                Log,
+                Function[{m},m^beta]
             ];
-        W[x_, A_] :=
-            A Sin[2 Pi (x + 1/4)]^2;
-        V[x_] :=
-            W[x, 1/2];
-        H[x_, p_, m_] :=
-            p^2/(2 m^alpha) + V[x] - g[m];
+        W = Function[{x,A},
+            A Sin[2 Pi (x + 1/4)]^2];
+        V = Function[{x},     
+        	W[x, 1/2]];
+        H = Function[{x,p,m},
+            p^2/(2 m^alpha) + V[x] - g[m]];
         Association[{"alpha" -> alpha, "beta" -> beta, "g[m]" -> g, 
           "W[x,A]" -> W, "V[x]" -> V, "H[x,p,m]" -> H}]
     ]
