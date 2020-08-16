98b790766a745bb19439dfde848a2769f80d92dd
fatimahsaleh <62715653+fatimahsaleh@users.noreply.github.com>
2020-08-16 21:18:09 +0300

    Adding the iterative solvers

--- MFGraphs/IterationFunction.m
+++ MFGraphs/IterationFunction.m
@@ -3,97 +3,76 @@
 (* Wolfram Language package *)
 
 
-(*Data = DataG[2];
-MFGEquations = DataToEquations[Data];*)
-
 (*ToRules @ Or @@ Reduce[# && Data[type2] && Data[type3]]&/@Data[type1] (*if Data[type1] has head Or*)
 *)
-(*jays = AssociationThread[
-   MFGEquations[
-    "BEL"], (MFGEquations["jvars"][AtHead[#]] - 
-       MFGEquations["jvars"][AtTail[#]] &) /@ MFGEquations["BEL"]];(*this is done in the variables level.*)
-*)
-
-F::usage =
-"";
-
-Intg::usage =
-"";
-
-
-Begin["`Private`"]
-F[j_?NumericQ, x_?NumericQ] :=
-    First@Values@FindRoot[Parameters["H[x,p,m]"][x, -j/(m^(1 - Parameters["alpha"])), m], {m, 1}];
-(*we are solving the h-j equation for m given that u_x = -j*m^(alpha-1) *)
-
-    
-Intg[j_?NumericQ] :=
-    - j NIntegrate[F[j, x]^(Parameters["alpha"] - 1), {x, 0, 1}] // Quiet
-(*this is the rhs of the integral of u_x from 0 to 1: ingetral_0^1 -j*m^(alpha-1) dx*)
-
-(*With[{j = #},
-	Module [{m},
-		m[x_?NumericQ] := F[j, x];
-		Intg[j]
-	]
-]&*)
-
-(*world = Association[{
-  
-   "EqAll" ->  MFGEquations["EqAll"],
-   "EqAllComp" ->  MFGEquations["EqAllComp"],
-   "lhs" -> 
-    Association[
-   (# -> MFGEquations["uvars"][AtHead[#]] - MFGEquations["uvars"][AtTail[#]] -  jays[#] &)/@ MFGEquations["BEL"]], 
-   "rhs" -> 
-    Association[
-     (# -> Intg(jays[#]) - jays[#] &) /@ MFGEquations["BEL"]]
-   }]*)
- 
-(*edge is #.....*) 
-
-(*TODO build a function that returns the result for the critical congestion case*)
 
 (*TODO learn how to document code!*)
 
 (*TODO make the iterative function work on all the values.*)
 
-(*G[world_Association] := 
-Function[ jays,  (*jays associates numeric values to the edges \
-of BG. Run "the code" in the critical congestion and feed it to G.*)
-    Module[{ItValues,EqItAll, EqAllRNZ, ResolvedRNZ, URNZ, JTJNZ, jvars, uvars, BG, js, jts, us, EqNoInt},
-    ItValues = 
-        And @@ ((Intg[jays[AtHead[#]] - jays[AtTail[#]]] == 
-        uvars[AtHead[#]] - uvars[AtTail[#]]) & /@ EdgeList[BG]);
-    EqItAll = EqNoInt && ItValues;
-    Print["Reducing ..."];
-    EqAllRNZ = Reduce[EqItAll, Reals];
-    Print["Eliminating js and jts ..."];
-    ResolvedRNZ = Resolve[EE[Join[js, jts], EqAllRNZ], Reals];
-    Print["Finding value function rules ... "];
-    URNZ = UU[ResolvedRNZ, us] // Flatten;
-    Print["Finding current and transition current rules ..."];
-    JTJNZ = UU[EqAllRNZ /. URNZ, Join[jts, js]] // Flatten;
-    jvars /. JTJNZ
-    ]
-    (*TODO what do we do when there are multiple solutions*)
-]
-DataToEquations[Data];
-*)
 
-(*TODO finish this which diogo started.*)
-(*G[world_Association] := 
-Function[ jays,
-	Module[{AssembledEquations},
-		AssembledEquations = And@@Map[world["lhs"][#] == world["rhs"][#][jays[#]], world["edges"]];
-		(*TODO solve and do all the things diogo said.*)
-	]
-]
+Data = DataG[2];
+MFGEquations = DataToEquations[Data];
 
-FixedPoint[G,0]
-*)
+EqEliminatorX[system_] := 
+  Module[{newrules},(*replace rules in system before solving*)
+   
+   newrules = 
+     Select[system, (Head[#] === Equal) &] && 
+         And @@ (globalrules /. Rule -> Equal) // Solve // First // 
+      Quiet;
+   (*Print[newrules]*);
+   globalrules =  newrules;(*replace in the rules before appending*)
+ 
+     system /. globalrules
+   ];
+  globalrules = {};
+AssociateTo[
+  MFGEquations, {"reduced" -> 
+    FixedPoint[EqEliminatorX, 
+     MFGEquations["EqAllComp"] && MFGEquations["EqAll"], 10], 
+   "globalrules" -> globalrules}];
+   
+   StartSolverX[Eqs_Association] :=
+ Module[{eqs, listeqs, rules},
+  eqs = DeleteDuplicates[
+    Select[Reduce[# && 
+         And @@ ((# == 0) & /@ Eqs["Nlhs"] /. Eqs["globalrules"]), 
+        Reals, Backsubstitution -> True] & /@ 
+      Eqs["Boo"], (# =!= False) &]];
+  listeqs = List @@ eqs;
+  rules = Solve[List @@ eqs, Reals];
+  If[Length[rules] > 1, 
+   Print["There is more than one set of NotFalse equations!"]; rules, 
+   rules]
+  ]
+  
+  startrulesX = StartSolverX[MFGEquations];
+AssociateTo[MFGEquations, 
+  "CriticalCongestionSolution" -> MFGEquations["globalrules"] /. 
+   First[startrulesX]];
+   
+   FixedPointSolverStepX[Eqs_Association][rules_] :=
+ 
+ Module[{system, nonlinear, newsolve},
+  nonlinear = 
+   And @@ (MapThread[(#1 == #2) &, {Eqs["Nlhs"] /. 
+        Eqs["globalrules"], (Eqs["Nrhs"] /. rules[[1]])}]);
+  Print[nonlinear];
+  system = nonlinear && Eqs["reduced"];
+  Print[system];
+  globalrules = Eqs["globalrules"];
+  newsolve = FixedPoint[EqEliminatorX, First@system, 10];
+  Print[newsolve];
+  globalrules
+  (*Try to use EqEliminatorX here. 
+  But rembemer it changes the globalrules variable (which is global)*)
+  ]
+  
+  FixedPointSolverStepX[MFGEquations][startrulesX]
+  
+   globalrules = {};
+FixedPoint[EqEliminatorX, First@nonlinearsystem, 10]
+globalrules
 
-(*EqValue[T_] = 
- And @@ ((T[#] == uvars[AtHead[#]] - uvars[AtTail[#]]) & /@ BEL); 
-*) (* TODO use the same reasoning to approach the more general cases.*)
 End[]
