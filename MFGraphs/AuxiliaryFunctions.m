b526c1d8ad4253f1aa93d3af30d885873aefe837
fatimahsaleh <62715653+fatimahsaleh@users.noreply.github.com>
2020-08-16 21:22:59 +0300

    Adding the integration function

--- MFGraphs/AuxiliaryFunctions.m
+++ MFGraphs/AuxiliaryFunctions.m
@@ -97,54 +97,7 @@ With[{j = #},
     		Intg[j]
     	]
    ] &;
-   
-   EqEliminatorX[system_] := 
-  Module[{newrules},(*replace rules in system before solving*)
-   
-   newrules = 
-     Select[system, (Head[#] === Equal) &] && 
-         And @@ (globalrules /. Rule -> Equal) // Solve // First // 
-      Quiet;
-   (*Print[newrules]*);
-   globalrules =  newrules;(*replace in the rules before appending*)
- 
-     system /. globalrules
-   ];
-   
-   StartSolverX[Eqs_Association] :=
- Module[{eqs, listeqs, rules},
-  eqs = DeleteDuplicates[
-    Select[Reduce[# && 
-         And @@ ((# == 0) & /@ Eqs["Nlhs"] /. Eqs["globalrules"]), 
-        Reals, Backsubstitution -> True] & /@ 
-      Eqs["Boo"], (# =!= False) &]];
-  listeqs = List @@ eqs;
-  rules = Solve[List @@ eqs, Reals];
-  If[Length[rules] > 1, 
-   Print["There is more than one set of NotFalse equations!"]; rules, 
-   rules]
-  ]
-FixedPointSolverStepX::usage = "takes a solution to the problem and \
-returns another. Example: from the solution to the critical \
-congestion problem, it gives a solution to the problem for which the \
-nonlinear equations have a right hand side pre-calculated."
-
-FixedPointSolverStepX[Eqs_Association][rules_] :=
- 
- Module[{system, nonlinear, newsolve},
-  nonlinear = 
-   And @@ (MapThread[(#1 == #2) &, {Eqs["Nlhs"] /. 
-        Eqs["globalrules"], (Eqs["Nrhs"] /. rules[[1]])}]);
-  Print[nonlinear];
-  system = nonlinear && Eqs["reduced"];
-  Print[system];
-  globalrules = Eqs["globalrules"];
-  newsolve = FixedPoint[EqEliminatorX, First@system, 10];
-  Print[newsolve];
-  globalrules
-  (*Try to use EqEliminatorX here. 
-  But rembemer it changes the globalrules variable (which is global)*)
-  ]
+  
   
   
 End[]
