423156b738ddcf96c20a8d784c579b26fcf6fb81
fatimahsaleh <62715653+fatimahsaleh@users.noreply.github.com>
2020-08-16 20:52:02 +0300

    Adding the solving functions

--- MFGraphs/AuxiliaryFunctions.m
+++ MFGraphs/AuxiliaryFunctions.m
@@ -76,4 +76,75 @@ EE = Exists[#1, #2] &;
 UU = Solve[#1, #2, Reals] &;
 
 RR = Reduce[#1, #2, Reals] &;
+
+F[j_?NumericQ, x_?NumericQ] :=
+      
+  First@Values@
+    FindRoot[
+     Parameters["H[x,p,m]"][x, -j/(m^(1 - Parameters["alpha"])), 
+      m], {m, 1}];
+(*we are solving the h-j equation for m given that u_x = \
+-j*m^(alpha-1) *)
+
+Intg[j_?NumericQ] :=
+     - j NIntegrate[
+    1/(F[j, x]^(1 - Parameters["alpha"])), {x, 0, 1}] // Quiet
+(*this is the rhs of the integral of u_x from 0 to 1: ingetral_0^1 \
+-j*m^(alpha-1) dx*)
+With[{j = #},
+   	Module [{m},
+    		m[x_?NumericQ] := F[j, x];
+    		Intg[j]
+    	]
+   ] &;
+   
+   EqEliminatorX[system_] := 
+  Module[{newrules},(*replace rules in system before solving*)
+   
+   newrules = 
+     Select[system, (Head[#] === Equal) &] && 
+         And @@ (globalrules /. Rule -> Equal) // Solve // First // 
+      Quiet;
+   (*Print[newrules]*);
+   globalrules =  newrules;(*replace in the rules before appending*)
+ 
+     system /. globalrules
+   ];
+   
+   StartSolverX[Eqs_Association] :=
+ Module[{eqs, listeqs, rules},
+  eqs = DeleteDuplicates[
+    Select[Reduce[# && 
+         And @@ ((# == 0) & /@ Eqs["Nlhs"] /. Eqs["globalrules"]), 
+        Reals, Backsubstitution -> True] & /@ 
+      Eqs["Boo"], (# =!= False) &]];
+  listeqs = List @@ eqs;
+  rules = Solve[List @@ eqs, Reals];
+  If[Length[rules] > 1, 
+   Print["There is more than one set of NotFalse equations!"]; rules, 
+   rules]
+  ]
+FixedPointSolverStepX::usage = "takes a solution to the problem and \
+returns another. Example: from the solution to the critical \
+congestion problem, it gives a solution to the problem for which the \
+nonlinear equations have a right hand side pre-calculated."
+
+FixedPointSolverStepX[Eqs_Association][rules_] :=
+ 
+ Module[{system, nonlinear, newsolve},
+  nonlinear = 
+   And @@ (MapThread[(#1 == #2) &, {Eqs["Nlhs"] /. 
+        Eqs["globalrules"], (Eqs["Nrhs"] /. rules[[1]])}]);
+  Print[nonlinear];
+  system = nonlinear && Eqs["reduced"];
+  Print[system];
+  globalrules = Eqs["globalrules"];
+  newsolve = FixedPoint[EqEliminatorX, First@system, 10];
+  Print[newsolve];
+  globalrules
+  (*Try to use EqEliminatorX here. 
+  But rembemer it changes the globalrules variable (which is global)*)
+  ]
+  
+  
 End[]
