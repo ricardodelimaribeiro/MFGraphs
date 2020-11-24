e03244b54491e5c785bb5a38392beb4261569231
fatimahsaleh <62715653+fatimahsaleh@users.noreply.github.com>
2020-11-24 22:12:08 +0300

    Adding new examples

--- MFGraphs/Examples/ExamplesData.m
+++ MFGraphs/Examples/ExamplesData.m
@@ -1,4 +1,174 @@
-(*2 edges entrance in the middle*)
+Test = Association[
+    (*One vertex*)
+    1 -> {
+        (*VL=*){1},
+        (*AM=*){{0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{1, U1}},
+        (*SwitchingCostsData=*){}},
+        
+    (*1 edge*)
+    2 -> {
+        (*VL=*){1, 2},
+        (*AM=*){{0, 1}, {0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{2, U1}},
+        (*SwitchingCostsData=*){}},
+        
+    (*2 edges*)
+    3 -> {
+        (*VL=*){1, 2, 3},
+        (*AM=*){{0, 1, 0}, {0, 0, 1}, {0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{3, U1}},
+        (*SwitchingCostsData=*){}},
+    
+    (*3 edges*)
+   4 -> {
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{4, U1}},
+        (*SwitchingCostsData=*){}},
+    
+    (*4 edges*)    
+    5 -> {
+        (*VL=*){1, 2, 3, 4, 5},
+        (*AM=*){{0, 1, 0, 0, 0}, {0, 0, 1, 0, 0}, 
+        	    {0, 0, 0, 1, 0}, {0, 0, 0, 0 ,1}, {0, 0, 0, 0 ,0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{5, U1}},
+        (*SwitchingCostsData=*){}},
+   
+   (*9 edges*)
+    6 -> {
+        (*VL=*){1,2,3,4,5,6,7,8,9,10},
+        (*AM=*){
+        {0,1,0,0,0,0,0,0,0,0},
+        {0,0,1,0,0,0,0,0,0,0},
+        {0,0,0,1,0,0,0,0,0,0},
+        {0,0,0,0,1,0,0,0,0,0},
+        {0,0,0,0,0,1,0,0,0,0},
+        {0,0,0,0,0,0,1,0,0,0},
+        {0,0,0,0,0,0,0,1,0,0},
+        {0,0,0,0,0,0,0,0,1,0},
+        {0,0,0,0,0,0,0,0,0,1},
+        {0,0,0,0,0,0,0,0,0,0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{10, U1}},
+        (*SwitchingCostsData=*){}},     
+       
+        
+     (**********)
+     
+        
+    (*Y 1-in 2-out 4 vertices no switching*)
+    7 -> {
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 0, 0}, {0, 0, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{3, U1}, {4, U2}},
+        (*SwitchingCostsData=*){}},
+        
+    (*Y 1-in 2-out 4 vertices with switching*)
+    8 -> {
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 0, 0}, {0, 0, 1, 1}, {0, 0, 0, 0}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{3, U1}, {4, U2}},
+        (*SwitchingCostsData=*){{1, 2, 3, S1}, {1, 2, 4, S2}, {3, 2, 1, S3}, {3, 2, 4, S4}, {4, 2, 1, S5}, {4, 2, 3, S6}}},
+        
+    (*Y 2-in 1-out 4 vertices no swithing*)
+    9 -> {
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 0, 0}, {0, 0, 0, 1}, {0, 1, 0, 0}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}, {3, I2}},
+        (*FinalCosts=*){{4, U1}},
+        (*SwitchingCostsData=*){}},
+        
+    (*Y 2-in 1-out 4 vertices with switching*)
+    10 -> {
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 0, 0}, {0, 0, 0, 1}, {0, 1, 0, 0}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}, {3, I2}},
+        (*FinalCosts=*){{4, U1}},
+        (*SwitchingCostsData=*){{1, 2, 4, S1}, {1, 2, 3, S2}, {3, 2, 1, S3}, {3, 2, 4, S4}, {4, 2, 1, S5}, {4, 2, 3, S6}}},
+   
+   (********)
+   
+        
+    (*Attraction Problem*)
+    11 -> {(*TODO try "one edge" with 3 or 4 vertices, the current is already set, there should be two iterations on the fixed point.*)
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{4, U2}}, 
+        (*SwitchingCostsData=*){
+            {1, 2, 4, S1}, 
+            {1, 2, 3, S2}, 
+            {3, 2, 1, S3},
+            {3, 2, 4, S4}, 
+            {4, 2, 1, S5}, 
+            {4, 2, 3, S6}, 
+            {1, 3, 4, S7}, 
+            {4, 3, 1, S8}, 
+            {1, 3, 2, S9}, 
+            {2, 3, 1, S10}, 
+            {3, 4, 2, S11}, 
+            {2, 4, 3, S12}, 
+            {2, 3, 4, S13}, 
+            {4, 3, 2, S14}, 
+            {3, 1, 2, S15}, 
+            {2, 1, 3, S16}}},
+   
+   (* Attraction without switching*)
+    12 -> {
+        (*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{4, U1}}, 
+        (*SwitchingCostsData=*){}},
+   
+    (* Attraction without edge in the middle*)
+    13 -> {
+    	(*VL=*){1, 2, 3, 4},
+        (*AM=*){{0, 1, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{4, U1}}, 
+        (*SwitchingCostsData=*){{1,2,4,S1},{1,3,4,S2}}},
+   
+   (*********)
+ 
+   (*triangle*)
+    14 -> {
+        (*VL=*){1, 2, 3},
+        (*AM=*){{0, 1, 0}, {0, 0, 1}, {1, 0, 0}},
+        (*DataIn=*){{1, I1}},
+        (*FinalCosts=*){{3, U1}},
+        (*SwitchingCostsData=*){{1, 2, 3, S1}, {3, 2, 1, S2}}},
+            
+        
+    (*Y 2-in 1-out 3 vertices with switching*)
+    15 -> {
+        (*VL=*){1, 2, 3},
+        (*AM=*){{0, 0, 0}, {1, 0, 0}, {1, 0, 0}},
+        (*DataIn=*){{2, I1}, {3, I2}},
+        (*FinalCosts=*){{1, U1}},
+        (*SwitchingCostsData=*){{2, 1, 3, S1}, {3, 1, 2,S2}}},
+        
+              
+    (*Error in the switching costs*)
+    16 -> {
+        (*VL*){1, 2, 3}, 
+        (*AM*){{0, 1, 0}, {0, 0, 1}, {0, 0, 0}}, 
+        (*DataIn*){{1, I1}}, 
+        (*FinalCosts*){{3, U1}}, 
+        (*SwiotchingCostsData*){{1, 3, 2, S1}}},
+        
+
+  
+       
+    (*2 edges entrance in the middle*)
     17 -> {
         (*VL=*) {1, 2, 3}, 
         (*AM*) {{0, 1, 0}, {0, 0, 1}, {0, 0, 0}}, 
@@ -58,6 +228,7 @@ DataG[n_] :=
     ](*To have a visual of the available examples, run: Table[DataToGraph[DataG[n]],{n,1,Length[Test]}]*)
                         
           
+
 
 
           
