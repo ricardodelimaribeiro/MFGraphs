d86734427967e57198f0d8ece6c61ba8dc6e4409
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2020-12-13 16:51:48 +0300

    trying to start some documentation

--- PacletInfo.m
+++ PacletInfo.m
@@ -1,11 +1,17 @@
 (* Paclet Info File *)
 
-(* created 2020.05.05*)
+(* created 2020/12/13*)
 
 Paclet[
-  Name -> "MFGraphs",
-  Version -> "0.0.1",
-  MathematicaVersion -> "6+",
-  Extensions -> {
-    {"Documentation", Language -> "English"}
-}]
+    Name -> "MFGraphs",
+    Version -> "0.0.1",
+    MathematicaVersion -> "6+",
+    Extensions -> 
+        {
+            {"Documentation", Resources -> 
+                {"Guides/MFGraphs.nb"}
+            , Language -> "English"}
+        }
+]
+
+
