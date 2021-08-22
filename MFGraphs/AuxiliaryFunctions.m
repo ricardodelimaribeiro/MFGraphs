efb7d5fc74e8a3392cff35aa13e4d4c20f17e70e
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2021-08-22 09:10:34 +0300

    Source format

--- MFGraphs/AuxiliaryFunctions.m
+++ MFGraphs/AuxiliaryFunctions.m
@@ -37,6 +37,7 @@ UU::usage =
 
 RR::usage = 
 "";*)
+
 F::usage =
 "for a numeric value of the current, this is the solution, m, for the H-J equation (u_x = -j)";
 
@@ -56,144 +57,131 @@ RoundValues::usage =
 "RoundValues Rounds the values in a Rule up to 10 decimal places"
 Begin["`Private`"]
 
-eStyle[colors_][pts_, DirectedEdge[x_, y_]] := {colors[[y]], 
-  Arrow[Line[pts, VertexColors -> {colors[[x]], colors[[y]]}]]}
+eStyle[colors_][pts_, DirectedEdge[x_, y_]] :=
+    {colors[[y]], 
+    Arrow[Line[pts, VertexColors -> {colors[[x]], colors[[y]]}]]}
 
+ColorNetworkByValueFunctionOperator[d2e1_Association][rules_] :=
+    Module[ {values, colors, vl, bg, uvars},
+        bg = Lookup[d2e1,"BG", Print["There is no basic graph"];
+                               Return[]];
+        vl = VertexList[bg];
+        uvars = Lookup[d2e1, "uvars", Print["There are no u variables"];
+                                      Return[]];
+        values = vl /. KeyMap[#[[1]] &, uvars /. rules];(*this works WITHOUT switching costs*)
+        (*Print[
+        values];*)
+        colors = ColorData["TemperatureMap"] /@ (values/Max[values]);
+        (*Print[colors];*)
+        GraphicsGrid[{
+          {HighlightGraph[
+            SetProperty[bg, EdgeShapeFunction -> eStyle[colors]], 
+            Thread[Style[vl, colors]], 
+            GraphLayout -> "SpringElectricalEmbedding"],
+          BarLegend["TemperatureMap", LegendMarkerSize -> 100(*, 
+            LegendLayout -> "ReversedRow"*)]}
+          }]
+    ]
 
-ColorNetworkByValueFunctionOperator[d2e1_Association][rules_] := 
- Module[{values, colors},
-  values = 
-   d2e1["VL"] /. 
-    KeyMap[#[[1]] &, 
-     d2e1["uvars"] /. 
-      rules];(*this works WITHOUT switching costs*)
-  (*Print[
-  values];*)
-  
-  colors = ColorData["TemperatureMap"] /@ (values/Max[values]);
-  (*Print[colors];*)
-  GraphicsGrid[{
-    {HighlightGraph[
-      SetProperty[d2e1["BG"], EdgeShapeFunction -> eStyle[colors]], 
-      Thread[Style[d2e1["VL"], colors]], 
-      GraphLayout -> "SpringElectricalEmbedding"],
-    BarLegend["TemperatureMap", LegendMarkerSize -> 100(*, 
-      LegendLayout -> "ReversedRow"*)]}
-    }]
-  ]
-
-
-
-
-
-RoundValues[x_?NumberQ] := Round[x, 10^-10]
-RoundValues[Rule[a_, b_]] := Rule[a, RoundValues[b]]
-RoundValues[x_List] := RoundValues /@ x
-RoundValues[x_Association]:= RoundValues /@ x
-	
+RoundValues[x_?NumberQ] :=
+    Round[x, 10^-10]
+    
+RoundValues[Rule[a_, b_]] :=
+    Rule[a, RoundValues[b]]
+    
+RoundValues[x_List] :=
+    RoundValues /@ x
+    
+RoundValues[x_Association] :=
+    RoundValues /@ x
+    
 SortAnds[oldxp_And, vars_] :=
-SortAnds[True, oldxp, vars]
+    SortAnds[True, oldxp, vars]
 
 SortAnds[oldxp_, vars_] :=
-oldxp
+    oldxp
 
 SortAnds[Newxp_, oldxp_, {}] :=
-Newxp && oldxp
+    Newxp && oldxp
 
 SortAnds[Newxp_, oldxp_And, vars_] :=
-With[{variablesofthemoment = Select[vars, ! FreeQ[Newxp, #] &]},
-  	If[variablesofthemoment === {},
-   		SortAnds[Newxp && Select[oldxp, ! FreeQ[#, First[vars]] &], Select[oldxp, FreeQ[#, First[vars]] &], Rest[vars]],
-   		SortAnds[Newxp && Select[oldxp, ! FreeQ[#, First[variablesofthemoment]] &],
-     		Select[oldxp, FreeQ[#, First[variablesofthemoment]] &], 
-    		Select[vars, (# =!= First[variablesofthemoment]) &]
-    	]
-   	]
-]
-(*if the head is not And*)
-
+    With[ {variablesofthemoment = Select[vars, ! FreeQ[Newxp, #] &]},
+        If[ variablesofthemoment === {},
+            SortAnds[Newxp && Select[oldxp, ! FreeQ[#, First[vars]] &], Select[oldxp, FreeQ[#, First[vars]] &], Rest[vars]],
+            SortAnds[Newxp && Select[oldxp, ! FreeQ[#, First[variablesofthemoment]] &],
+              Select[oldxp, FreeQ[#, First[variablesofthemoment]] &], 
+             Select[vars, (# =!= First[variablesofthemoment]) &]
+            ]
+        ]
+    ]
+    
 SortAnds[Newxp_, oldxp_, vars_] :=
-Newxp && oldxp
+    Newxp && oldxp
 
 AtHead[a_ \[DirectedEdge] b_] :=
-{b, a \[DirectedEdge] b};
-(*AtHead[edge_] := {edge[[2]], edge}
-AtTail[edge_] := {edge[[1]], edge}
-*)
+    {b, a \[DirectedEdge] b}
+
 AtTail[a_ \[DirectedEdge] b_] :=
-{a, a \[DirectedEdge] b};
+    {a, a \[DirectedEdge] b}
 
 TransitionsAt[G_, k_] :=
-Prepend[#, k] & /@ Permutations[IncidenceList[G, k], {2}];
+    Prepend[#, k] & /@ Permutations[IncidenceList[G, k], {2}]
     
 OtherWay[{c_, a_ \[DirectedEdge] b_}] :=
-{If[ c === a,
-	b,
-	a
-], a \[DirectedEdge] b}
-(*OtherWay[{a_, a_ \[DirectedEdge] b_}] :=
-    {b, a \[DirectedEdge] b}
-OtherWay[{b_, a_ \[DirectedEdge] b_}] :=
-    {a, a \[DirectedEdge] b}
-OtherWay[{c_, edge_}] := 
-	If[c === edge[[2]], 
-		AtTail[edge],
-		AtHead[edge]
-	]*)
+    {If[ c === a,
+         b,
+         a
+     ], a \[DirectedEdge] b}
 
 triple2path[{a_, b_, c_}, G_] :=
-Module[ {EL = EdgeList[G]},
-    If[ SubsetQ[AdjacencyList[G, b], {a, c}],
-        If[ MemberQ[EL, a \[DirectedEdge] b],
-            If[ MemberQ[EL, b \[DirectedEdge] c],
-                {b, a \[DirectedEdge] b, b \[DirectedEdge] c},
-                {b, a \[DirectedEdge] b, c \[DirectedEdge] b}
-            ],
-            If[ MemberQ[EL, b \[DirectedEdge] a],
+    Module[ {EL = EdgeList[G]},
+        If[ SubsetQ[AdjacencyList[G, b], {a, c}],
+            If[ MemberQ[EL, a \[DirectedEdge] b],
                 If[ MemberQ[EL, b \[DirectedEdge] c],
-                    {b, b \[DirectedEdge] a, b \[DirectedEdge] c},
-                    {b, b \[DirectedEdge] a, c \[DirectedEdge] b}
+                    {b, a \[DirectedEdge] b, b \[DirectedEdge] c},
+                    {b, a \[DirectedEdge] b, c \[DirectedEdge] b}
+                ],
+                If[ MemberQ[EL, b \[DirectedEdge] a],
+                    If[ MemberQ[EL, b \[DirectedEdge] c],
+                        {b, b \[DirectedEdge] a, b \[DirectedEdge] c},
+                        {b, b \[DirectedEdge] a, c \[DirectedEdge] b}
+                    ]
                 ]
-            ]
-        ],
-        StringJoin["\nThere is no path from ", ToString[a], " to ", ToString[b], " to ", ToString[c]]
-	]
-]
-           
-(*EE = Exists[#1, #2] &;
-UU = Solve[#1, #2, Reals] &;
-RR = Reduce[#1, #2, Reals] &;*)
+            ],
+            StringJoin["\nThere is no path from ", ToString[a], " to ", ToString[b], " to ", ToString[c]]
+        ]
+    ]
 
 F[j_?NumericQ, x_?NumericQ] :=
-First @ Values @ FindRoot[Parameters["H[x,p,m]"][x, -j/(m^(1 - Parameters["alpha"])),m], {m, 1}];
+    First @ Values @ FindRoot[Parameters["H[x,p,m]"][x, -j/(m^(1 - Parameters["alpha"])),m], {m, 1}];
 (*we are solving the h-j equation, H[x,u_x,m] == 0, for m, given that u_x = -j*m^(alpha-1) 
 The hamiltonian depends on g[m]!*)
 
 (* is m becoming zero?*)
 Intg[j_?NumericQ] :=
-If[j == 0.|| j == 0 , 
-	0. ,
-   	-j Chop[NIntegrate[ 1/(F[j, x]^(1 - Parameters["alpha"])), {x, 0, 1}]] // Quiet
-]
+    If[ j == 0.|| j == 0,
+        0.,
+        -j Chop[NIntegrate[ 1/(F[j, x]^(1 - Parameters["alpha"])), {x, 0, 1}]] // Quiet
+    ]
 (*this is the rhs of the integral of u_x from 0 to 1: ingetral_0^1 \
 -j*m^(alpha-1) dx*)
 
 M[j_?NumericQ, x_?NumericQ, edge_] :=
-If[j == 0.|| j == 0 , 
-	0. ,
-	Values @ First @ FindRoot[H[x, -j/(m^(1 - alpha)),m, edge], {m, 1}]
-]
+    If[ j == 0.|| j == 0,
+        0.,
+        Values @ First @ FindRoot[H[x, -j/(m^(1 - alpha)),m, edge], {m, 1}]
+    ]
 
 U[x_?NumericQ , edge_, Eqs_Association] :=
-If[Eqs["jays"][edge] == 0.|| Eqs["jays"][edge] == 0 , 
-	Eqs["uvars"][AtTail[edge]] ,
-	Eqs["uvars"][AtTail[edge]] - Eqs["jays"][edge] NIntegrate[M[Eqs["jays"][edge], y, edge]^(alpha - 1), {y, 0, x}]
-]
-		
-IntM[j_?NumericQ, edge_] :=	
-If[j == 0.|| j == 0 , 
-	0. ,
-   	-j NIntegrate[ M[j, x, edge]^(alpha-1), {x, 0, 1}] // Quiet
-]
+    If[ Eqs["jays"][edge] == 0.|| Eqs["jays"][edge] == 0,
+        Eqs["uvars"][AtTail[edge]],
+        Eqs["uvars"][AtTail[edge]] - Eqs["jays"][edge] NIntegrate[M[Eqs["jays"][edge], y, edge]^(alpha - 1), {y, 0, x}]
+    ]
+        
+IntM[j_?NumericQ, edge_] :=
+    If[ j == 0.|| j == 0,
+        0.,
+        -j NIntegrate[ M[j, x, edge]^(alpha-1), {x, 0, 1}] // Quiet
+    ]
 (*TODO : from previous guess, look at rhs, if m is negative, the corresponding j needs to be 0*)  
 End[]
