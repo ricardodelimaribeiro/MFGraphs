e3783bc7172653a8aebbc498b8dd71f5f5d40418
ricardodelimaribeiro <ricardodelimaribeiro@gmail.com>
2022-05-24 11:09:15 +0300

    Update NonLinearSolver.m
    Finished the non linear solver! now it tells us how many times it iterated.
    
    also, the function that checks if we have a solution is giving good results.
    
    we can improve the accuracy of the code by changing the accuracy of the RoundValues function.

--- MFGraphs/NonLinearSolver.m
+++ MFGraphs/NonLinearSolver.m
@@ -1,48 +1,67 @@
 (*Wolfram Language package*)
 Get["/Users/ribeirrd/eclipse-workspace/MFGraphs/MFGraphs/D2E2.m"];
 NonLinear[Eqs_] :=    
-  Module[{AssoCritical, PreEqs, AssoNonCritical, js}, 
+  Module[{AssoCritical, PreEqs, AssoNonCritical, js, InitRules, MaxIter = 50}, 
    PreEqs = If[KeyExistsQ[Eqs,"InitRules"], Eqs, MFGPreprocessing[Eqs]];
    AssoCritical = Lookup[Eqs, "AssoCritical", CriticalCongestionSolver[Eqs]];
    js = Lookup[Eqs, "js",$Failed];
-   AssoNonCritical = FixedPoint[NonLinearStep[PreEqs], KeyTake[AssoCritical,js], 4];
+   AssoNonCritical = FixedPointList[NonLinearStep[PreEqs], KeyTake[AssoCritical,js], MaxIter];
+   Print["Iterated ", Length[AssoNonCritical], " times out of ", MaxIter];
+   AssoNonCritical = AssoNonCritical//Last;
    InitRules = Lookup[Eqs, "InitRules", $Failed];
-   costpluscurrents = Lookup[Eqs, "costpluscurrents",$Failed];
+   AssoNonCritical = MFGSystemSolver[Eqs][AssoNonCritical];
+   (*Print[AssoNonCritical];*)
+   (*costpluscurrents = Lookup[Eqs, "costpluscurrents",$Failed];
    Print[InitRules];
-   Print[InitRules/.costpluscurrents/.AssoNonCritical];
+   Print[InitRules/.costpluscurrents/.AssoNonCritical];*)
    Join[PreEqs, Association["AssoNonCritical" -> AssoNonCritical]]
    ];
 
 NonLinearStep[Eqs_][approxJs_] := 
 	Module[{approx, js},
-		Print[approxJs];
+		(*Print[approxJs];*)
 		js = Lookup[Eqs, "js",$Failed];
 		approx = MFGSystemSolver[Eqs][approxJs];
 		approx = KeyTake[approx, js];
-		Print["step: ",approx];
+		(*Print["step: ",approx];*)
 		approx
 	];
-	
+	(*TODO: assoc is given the right way? can we get it from the Eqs?*)
 IsNonLinearSolution[Eqs_][assoc_]:=
 Module[{EqEntryIn, EqValueAuxiliaryEdges, EqSwitchingByVertex, EqCompCon, 
     EqBalanceSplittingCurrents, EqCurrentCompCon, EqTransitionCompCon,
     EqPosJs, EqPosJts}, 
-     EqEntryIn = Lookup[Eqs, "EqEntryIn", $Failed];
+     EqEntryIn = And @@ Lookup[Eqs, "EqEntryIn", $Failed];
      EqCompCon = Lookup[Eqs, "EqCompCon", $Failed];
      EqCurrentCompCon = Lookup[Eqs, "EqCurrentCompCon", $Failed];
      EqTransitionCompCon = Lookup[Eqs, "EqTransitionCompCon", $Failed];
      EqPosJs = Lookup[Eqs, "EqPosJs", $Failed];
      EqPosJts = Lookup[Eqs, "EqPosJts", $Failed];
-     EqSwitchingByVertex = Lookup[Eqs, "EqSwitchingByVertex", $Failed];
+     EqSwitchingByVertex = And @@ Lookup[Eqs, "EqSwitchingByVertex", $Failed];
      EqBalanceSplittingCurrents = Lookup[Eqs, "EqBalanceSplittingCurrents", $Failed];
      EqValueAuxiliaryEdges = Lookup[Eqs, "EqValueAuxiliaryEdges", $Failed];
-	(Print[#/.assoc]&/@ {EqEntryIn, EqValueAuxiliaryEdges, EqSwitchingByVertex, EqCompCon, 
-    EqBalanceSplittingCurrents, EqCurrentCompCon, EqTransitionCompCon,
-    EqPosJs, EqPosJts});
-    assoc
+     EqGeneral = Lookup[Eqs, "EqGeneral", $Failed];
+     Nrhs = Lookup[Eqs, "Nrhs", $Failed];
+     Nlhs = Lookup[Eqs, "Nlhs", $Failed];
+     costpluscurrents = Lookup[Eqs, "costpluscurrents", $Failed];
+     js = Lookup[Eqs, "js", $Failed];
+     
+     Print["EqEntryIn: ", EqEntryIn, "\t", EqEntryIn/.assoc];
+     Print["EqValueAuxiliaryEdges: ", EqValueAuxiliaryEdges, "\t",EqValueAuxiliaryEdges/.assoc];
+     Print["EqCompCon: ", EqCompCon, "\t", EqCompCon/.assoc];
+     Print["EqBalanceSplittingCurrents: ", EqBalanceSplittingCurrents, "\t",EqBalanceSplittingCurrents/.assoc];
+     Print["EqCurrentCompCon: ", EqCurrentCompCon, "\t",EqCurrentCompCon/.assoc];
+     Print["EqTransitionCompCon: ", EqTransitionCompCon, "\t",EqTransitionCompCon/.assoc];
+     Print["EqPosJs: ", EqPosJs, "\t",EqPosJs/.assoc];
+     Print["EqPosJts: ", EqPosJts, "\t",EqPosJts/.assoc];
+     Print["EqSwitchingByVertex: ", EqSwitchingByVertex, "\t",Simplify/@(EqSwitchingByVertex/.assoc)];
+     Print["Nlhs: ", Nlhs, "\t", N[Nlhs/.assoc]];
+     Print["Nrhs: ", Nrhs, "\t", Nrhs/.assoc];
+     Print["Max error for non-linear solution: ", Max[N[Nlhs/.assoc]-(Nrhs/.assoc)]];
+     assoc
 	];
 	
-RoundValues[x_?NumberQ] := Round[x, 10^-10]
+RoundValues[x_?NumberQ] := Round[x, 10^-11]
 
 RoundValues[Rule[a_, b_]] := Rule[a, RoundValues[b]]
 
